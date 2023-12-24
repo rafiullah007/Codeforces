80f39a5e2bc7edbfedf5890aea46118752e03d6a
rafiullah007 <136167854+rafiullah007@users.noreply.github.com>
2023-12-24 14:56:58 +0600

    Add files via upload

---
+++ 59A.Word.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    int i,up=0,low=0;
+    for(i=0;i<s.size();i++)
+    {
+        if(isupper(s[i]))
+        {
+            up++;
+        }
+        else
+        {
+            low++;
+        }
+    }
+    if(up<low)
+    {
+        for(i=0;i<s.size();i++)
+        {
+            s[i]=tolower(s[i]);
+        }
+        cout<<s<<endl;
+    }
+    else if(up>low)
+    {
+        for(i=0;i<s.size();i++)
+        {
+            s[i]=toupper(s[i]);
+        }
+        cout<<s<<endl;
+    }
+    else if(up==low)
+    {
+        for(i=0;i<s.size();i++)
+        {
+            s[i]=tolower(s[i]);
+        }
+        cout<<s<<endl;
+    }
+}
