f5f5d975eebec96ea7a1e35799f18774c4ff7d76
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2023-12-27 00:09:53 +0600

    Update 520A.Pangram.cpp

--- 520A.Pangram.cpp
+++ 520A.Pangram.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
+
+    /*
+
+    MD. RAFIULLAH AL NAIM
+    UNIVERSITY : UNIVERSITY OF ASIA PACIFIC
+    STUDENT ID : 22101150 (UAP,DHAKA,BANGLADESH)
+    COLLEGE : ADAMJEE CANTONMENT COLLEGE
+    STUDENT ID : 181063 (ACC,DHAKA,BANGLADESH)
+
+    */
+
 int main()
 {
     int n;
