dc528d222ef3a84af9c091429aee88acb100278d
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2023-12-27 00:12:14 +0600

    Update 71A. Way Too Long Words.cpp

--- 71A. Way Too Long Words.cpp
+++ 71A. Way Too Long Words.cpp
@@ -1,5 +1,16 @@
 #include<stdio.h>
 #include<string.h>
+
+    /*
+
+    MD. RAFIULLAH AL NAIM
+    UNIVERSITY : UNIVERSITY OF ASIA PACIFIC
+    STUDENT ID : 22101150 (UAP,DHAKA,BANGLADESH)
+    COLLEGE : ADAMJEE CANTONMENT COLLEGE
+    STUDENT ID : 181063 (ACC,DHAKA,BANGLADESH)
+
+    */
+
 int main()
 {
     int t;
