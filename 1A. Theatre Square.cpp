14a4fd5ac51bd990a6cf0a6f7dbc1e96261ce233
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2023-12-27 00:08:14 +0600

    Update 1A. Theatre Square.cpp

--- 1A. Theatre Square.cpp
+++ 1A. Theatre Square.cpp
@@ -2,6 +2,17 @@
 #include <cmath>
 using namespace std;
 
+    /*
+
+    MD. RAFIULLAH AL NAIM
+    UNIVERSITY : UNIVERSITY OF ASIA PACIFIC
+    STUDENT ID : 22101150 (UAP,DHAKA,BANGLADESH)
+    COLLEGE : ADAMJEE CANTONMENT COLLEGE
+    STUDENT ID : 181063 (ACC,DHAKA,BANGLADESH)
+
+    */
+
+
 int main()
 {
     long double a, n, m, temp;
