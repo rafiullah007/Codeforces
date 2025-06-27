3de265e3eba0c8723fc2231b7fdf32b0d8bd2eea
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2025-06-27 23:20:51 +0600

    Update 510A. Fox And Snake.py

--- 510A. Fox And Snake.py
+++ 510A. Fox And Snake.py
@@ -2,11 +2,11 @@ n, m = map(int, input().split())
 
 for i in range(1, n + 1):
     if i % 2 == 1:
-        # Full row of '#'
+        # row fully #
         print('#' * m)
     elif i % 4 == 0:
-        # '#' at the beginning
+        # at the beginning #
         print('#' + '.' * (m - 1))
     else:
-        # '#' at the end
+        # at the end #
         print('.' * (m - 1) + '#')
