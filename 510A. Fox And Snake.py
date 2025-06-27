2064181050d549739d9cc71f0a7a94907635285f
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2025-06-27 23:19:57 +0600

    Create 510A. Fox And Snake.py

---
+++ 510A. Fox And Snake.py
@@ -0,0 +1,12 @@
+n, m = map(int, input().split())
+
+for i in range(1, n + 1):
+    if i % 2 == 1:
+        # Full row of '#'
+        print('#' * m)
+    elif i % 4 == 0:
+        # '#' at the beginning
+        print('#' + '.' * (m - 1))
+    else:
+        # '#' at the end
+        print('.' * (m - 1) + '#')
